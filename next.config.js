f1d72eb8e189b58ab12dccf07e534503ab207094
index.html <4thgroundcompany@gmail.com>
2026-06-18 06:43:03 +0200

    Update Content Security Policy for Paystack integration

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "frame-src 'self' https://gumroad.com https://*.gumroad.com https://iframe.mediadelivery.net https://player.mediadelivery.net; connect-src 'self' https://gumroad.com https://*.gumroad.com https://api.gumroad.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval';"
+            value: "frame-src 'self' https://js.paystack.co https://checkout.paystack.com https://iframe.mediadelivery.net https://player.mediadelivery.net; connect-src 'self' https://api.paystack.co https://checkout.paystack.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co;"
           }
         ]
       }
