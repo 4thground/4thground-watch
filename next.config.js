cf702ace73a706907bd86c516af89c0ae8fd1188
index.html <4thgroundcompany@gmail.com>
2026-06-25 12:27:36 +0200

    Update Content Security Policy to include Payhip

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "frame-src 'self' https://js.paystack.co https://checkout.paystack.com https://iframe.mediadelivery.net https://player.mediadelivery.net; connect-src 'self' https://api.paystack.co https://checkout.paystack.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co;"
+            value: "frame-src 'self' https://js.paystack.co https://checkout.paystack.com https://iframe.mediadelivery.net https://player.mediadelivery.net https://payhip.com https://*.payhip.com; connect-src 'self' https://api.paystack.co https://checkout.paystack.com https://payhip.com https://*.payhip.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline' https://payhip.com https://*.payhip.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co https://payhip.com https://*.payhip.com;"
           }
         ]
       }
