d485555e38778301d2ade49eb5644dffad70755b
index.html <4thgroundcompany@gmail.com>
2026-06-25 12:46:46 +0200

    Update Content-Security-Policy in next.config.js
    Removed Paystack URLs from Content-Security-Policy.

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "frame-src 'self' https://js.paystack.co https://checkout.paystack.com https://iframe.mediadelivery.net https://player.mediadelivery.net https://payhip.com https://*.payhip.com; connect-src 'self' https://api.paystack.co https://checkout.paystack.com https://payhip.com https://*.payhip.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline' https://payhip.com https://*.payhip.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co https://payhip.com https://*.payhip.com;"
+            value: "frame-src 'self' https://iframe.mediadelivery.net https://player.mediadelivery.net https://payhip.com https://*.payhip.com; connect-src 'self' https://payhip.com https://*.payhip.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline' https://payhip.com https://*.payhip.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://payhip.com https://*.payhip.com;"
           }
         ]
       }
